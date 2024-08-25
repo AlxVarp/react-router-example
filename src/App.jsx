f81d1ef9ed23fd163ec1b9379bcc6ee97f2237c3
Alxvarp <ingvargasalexander@gmail.com>
2024-08-25 05:09:30 +0200

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
     return (
         <div>
-            <h1>App</h1>
+            <h1>App Number 1</h1>
             <Outlet/>
             <div className="btn-container">
                 <Button title="Home" onHandleClick={onHandleClickButton} pathToGo="/" />
